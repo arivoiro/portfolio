53c90c1162fa714e441a99490ec6ff295118768e
Allan Rivoiron <allan.rivoiron02@gmail.com>
2023-11-06 22:50:49 +0100

    changed language for header titles

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header sharedData={this.state.sharedData.basic_info} />
+        <Header sharedData={this.state.sharedData.basic_info} resumeData={this.state.resumeData.basic_info} />
         <div className="col-md-12 mx-auto text-center language">
           <div
             onClick={() =>
